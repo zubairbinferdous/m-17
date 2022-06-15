67b72175ef9e9052445823366e3fae221943fe69
zubairbinferdous <zubairbinf@gmail.com>
2022-06-15 22:40:30 +0600

    ok

--- first.js
+++ first.js
@@ -1,2 +1,11 @@
-
 console.log("hello world");
+
+// use javascript in html and css
+// Brendan Eich creat js ** Netscape web browser give some animation, interpreted or script language //
+// INSPECT USE JAVASCRIPT
+// compile and interpreted language
+// js file .js
+// codepen 
+// notion 
+
+
--- variable.js
+++ variable.js
@@ -1,23 +1,181 @@
-var school = "habiab school and college" ;
-const Name = "zubi" ;
-let age = 29;
-age = 31;
-age = 40;
-const yourName = 'habib khan';
-console.log("school::",school, "name::",yourName , "age:", age);
+
+// 1 variable
+// 2 operator + - * ++  -- / 
+// 3 data type -> string , number ,  booleans , array , object 
+// 4 loop 
+// 5 condition 
+// 6 function 
+// 7 array 
+// 8 dom
+
+// variabe will be change 
+// variable use for information /data ;
+// var then name ;
+// five thing you need to call variable in js 1.var , 2.name , 3.= , 4.value , 5.;
+// string '' "" `` ;
+// var key word VAR not Var case sensitive
+// key word name not allow 
+// var name to = 'tom'; one word  not use  var  "name" = 'zubair'; ...
+// var username 99name   = userName or user_name or p45 ; camels case
+// var save when white color gone.
+// alt shift downArro
+// 
+
+// var school = "habiab school and college" ;
+// const Name = "zubi" ;
+// let age = 29;
+// const yourName = 'habib khan';
+// var pass = true; // js building key word.
+// console.log("school::",school, "name::",yourName , "age:", age);
+
+// operatin 
+
+// var priceOne = 100;
+// var priceTwo = 10;
+// var sum =  priceOne + priceTwo;
+// var different =  priceOne - priceTwo;
+// var multiplication =  priceOne * priceTwo;
+// var division =  priceOne / priceTwo;
+// priceOne += 200;
+// priceTwo -= 5;
+
+// age = 22;
+// age++;
+// age--;
 
 
-function mySum(a,b){
-    return a*b;
-}
-const Sum = mySum(2,3);
-console.log(Sum);
+// console.log(sum);
+// console.log(different);
+// console.log(multiplication);
+// console.log(division);
 
 
-var otherName = ["me", "you", "we"];
-for (let i = 0; i < otherName.length; i++) {
-    const element = otherName[i];
-    console.log(element);
-}
+// var PhoneQuantity = 7;
+// var applePrice = 900;
+// var samsunPrice = 700; 
+// var total = applePrice + samsunPrice;
+// var priceDifferent = applePrice - samsunPrice;
+// var totalApple = applePrice * PhoneQuantity;
+// console.log(applePrice + samsunPrice);
+// console.log(total);
 
-var youtube = 'ok';
+// var money = 100;
+// var phonePrice = 5;
+// var phone = money / phonePrice;
+// console.log(phone);
+
+// money += 200;
+// money -= 20; 
+
+// function mySum(a,b){
+//     return a*b;
+// }
+// const Sum = mySum(2,3);
+// console.log(Sum);
+
+
+
+// variable -> array -> condition -> function.
+// concatenation 
+
+var firstName = 'zubi';
+var lastName  =  'khan';
+var fullName  = firstName + ' ' +lastName;
+// console.log(fullName);
+
+// integer 
+var numOne = 10;
+// float
+var numTwo  =  "20.86";
+
+var inputNmber = '31';
+var number  = numOne+numTwo;
+var input = parseInt(numTwo);
+inputNmber = parseInt(inputNmber);
+numTwo = parseFloat(numTwo);
+
+// console.log(input);
+// console.log(inputNmber);
+// console.log(numTwo);
+
+// var tShirtPrice = 200;
+// var tShirtPrice2 = '200';
+// var tShirtPrice4 = 3.1234567;
+// var tShirtPrice3 = true;
+// var variable;
+// var today = new Date();
+// console.log(typeof tShirtPrice)
+// console.log(typeof tShirtPrice2)
+// console.log(typeof tShirtPrice3)
+// console.log(typeof variable)
+// console.log(today);
+// console.log(typeof today);                            // 2-3 din por janbo 
+// console.log(tShirtPrice4.toFixed(2));
+
+
+// condition 
+
+// var isFoodReady = 20;
+
+// if (isFoodReady == true) {
+//     console.log('ok i will eat now');
+// }else if(isFoodReady == 20){
+//     console.log('hahha')
+// }
+// else{
+//     console.log( 'ok not now')
+// }
+
+
+// variable -> array -> condition 
+
+// var bottleColor = 'yellow';
+// var waterQuantity = 10;
+// var isFull = true;
+
+// //
+
+var iteams = ['ball' , 'paper' , 'pen' , 'bottle'];
+
+iteams.indexOf('paper');
+iteams.indexOf('zubi');
+iteams.push('watch');
+iteams.pop();
+
+console.log(iteams.indexOf('paper'));
+console.log(iteams.pop('ball'));
+console.log(iteams);
+
+// if(iteams.lenght > 3){
+//     console.log('ok')
+// }else(
+//     console.log('not ok bhai')
+// )
+
+
+// while loop 
+
+// var roastGiven = 0;
+
+// while ( roastGiven < 7) {
+//     console.log( roastGiven , ' Rost den ');
+//     roastGiven++
+//     console.log( roastGiven);
+// }
+
+
+
+
+// var otherName = ["me", "you", "we"];
+// for (let i = 0; i < otherName.length; i++) {
+//     const element = otherName[i];
+//     console.log(element);
+// }
+
+
+// function test() {
+//     console.log('ok');
+// }
+// test();
+
+
